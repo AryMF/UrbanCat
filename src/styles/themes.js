3d74fe631ffc849188897db02514298eb0486374
AryMF <ariadne.hp@gmail.com>
2020-06-10 20:32:47 -0500

    :lipstick: Adding GlobalStyle and themes

---
+++ src/styles/themes.js
@@ -0,0 +1,19 @@
+const Light = {
+	isLight: true,
+	background: '#F6EDE8',
+	mainFontColor: '#5F5A56',
+	secondFondColor: '#000000',
+	mainAccentColor: '#B94B3E',
+	secondAccentColor: '#F1BD8E',
+};
+
+const Dark = {
+	isLight: true,
+	background: '#214255',
+	mainFontColor: '#FFFFFF',
+	secondFondColor: '#000000',
+	mainAccentColor: '#B94B3E',
+	secondAccentColor: '#B94B3E',
+};
+
+export { Light, Dark };

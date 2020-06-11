cb3201b7c126f4d10aa29cabac723223cb6e1237
Laura <laurarroncbf@gmail.com>
2020-06-11 15:57:38 -0500

    :construction: small fix
    correcting typo

--- src/contexts/fetchInfo.js
+++ src/contexts/fetchInfo.js
@@ -36,30 +36,6 @@ const apiCallwithTokenFunction = (url = '', http_method = 'GET') => {
     return fetch(url, requestMethod);
 };
 
-
-
-
-
-
-var myHeaders = new Headers();
-myHeaders.append("Cookie", "ARRAffinity=1fa22e5e18dd42b9d22d6c2c620b562f117da68736f4d32b741538a2d054d95c");
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
-fetch("https://genericapiv1.azurewebsites.net/v1/shop/promotions", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-
-
-
-
-
 const test = fetch(URI, {method: 'GET'})
                 .then( (response) => {
                     if(response.ok){

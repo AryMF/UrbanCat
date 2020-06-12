4b379c717d4546ef8190933a3a55f1aee45f12ce
Laura <laurarroncbf@gmail.com>
2020-06-11 21:29:58 -0500

    :construction: GET method functions
    funtions to GET data from categories, promotions and products

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import getProductsData from './contexts/fetchInfo.js';
 
 function App() {
+  getProductsData();
   return (
     <div className="App">
       <header className="App-header">
--- src/contexts/fetchInfo.js
+++ src/contexts/fetchInfo.js
@@ -20,35 +20,88 @@ const apiCallFunction = (url = '', http_method = 'GET') => {
 };
 
 
-//Calls with token
-const createHeaderToken = (type) => {
-    const myHeaders = new Headers();
-    myHeaders.append('authtoken', '???');
-	
-	return {
-		method: type,
-		headers: myHeaders,
-	};
+//Fetching categories
+const getCategoriesData = () => {
+    return new Promise((resolve, reject) => {
+        apiCallFunction(URI + categories, 'GET')
+        .then(response => response.json())
+        .then(result => {
+
+            const categories = {
+              'id': result.categoryID,
+              'category': result.description,
+              'pic': result.picture,
+              'date': result.createDate,
+              'status': result.isActive,
+              'update': result.updateDate,
+            }
+
+            resolve (categories);
+        })
+        .catch(error => {
+            console.log('error', error);
+            reject (error);
+        });
+    });
 };
 
-const apiCallwithTokenFunction = (url = '', http_method = 'GET') => {
-    const requestMethod = createHeaderToken(http_method);
-    return fetch(url, requestMethod);
+//Fetching promotions
+const getPromotionsData = () => {
+    return new Promise((resolve, reject) => {
+        apiCallFunction(URI + promotions, 'GET')
+        .then(response => response.json())
+        .then(result =>{
+    
+            const promotions = {
+               'promoId': result.promotionID,
+               'description': result.description,
+               'discount': result.persentDiscount,
+               'startDate': result.startDate,
+               'endDate': result.endDate,
+               'createDate': result.createDate,
+               'updateDate': result.updateDate,
+               'isActive': result.isActive,
+               'pic': result.picture,
+            }
+
+            resolve (promotions);
+        })
+        .catch(error => {
+            console.log('error', error);
+            reject (error);
+        });
+    });
 };
 
-const test = fetch(URI, {method: 'GET'})
-                .then( (response) => {
-                    if(response.ok){
-                        return response.json();
-                    } else {
-                        throw new Error('BAD STUFF');
-                    }
-                })
-                .then( (jsonData) => {
-                    console.log(jsonData);
-                })
-                .catch( (err) => {
-                    console.log('ERROR', err.mesage);
-            });
+//Fetching products
+const getProductsData = () => {
+    return new Promise((resolve, reject) => {
+        apiCallFunction(URI + products, 'GET')
+        .then(response => response.json())
+        .then(result =>{
+            console.log(result)
+            const products = {
+               'productId': result.productId,
+               'description': result.description,
+               'categoryId': result.categoryId,
+               'colors': result.colors,
+               'sizes': result.sizes,
+               'price': result.price,
+               'pic': result.picture,
+               'createDate': result.createDate,
+               'updateDate': result.updateDate,
+               'isActive': result.isActive,
+            }
 
-export { test as default };
+            resolve (promotions);
+        })
+        .catch(error => {
+            console.log('error', error);
+            reject (error);
+        });
+    });
+};
+
+
+
+export default getProductsData;
